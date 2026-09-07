bc4eb5d97ca8a2cde58730238a91cabad564a92d
Burbank <burbank@users.noreply.github.com>
2026-09-07 17:07:23 +0000

    Tighten icon: M is 15% larger, less white around it.

--- Scripts/build-app.sh
+++ Scripts/build-app.sh
@@ -71,9 +71,9 @@ cat > "$CONTENTS/Info.plist" <<PLIST
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>0.3.2</string>
+	<string>0.3.3</string>
 	<key>CFBundleVersion</key>
-	<string>14</string>${ICON_PLIST}
+	<string>15</string>${ICON_PLIST}
 	<key>LSMinimumSystemVersion</key>
 	<string>14.0</string>
 	<key>NSHighResolutionCapable</key>
